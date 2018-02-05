b603f02ccc2ab4751b923693a5ab27840e2d22a0
Mamoru Hirasaki <hirasaki1985@gmail.com>
2018-02-05 10:04:39 +0900

    Update db_initialize.sql

--- initialize/db_initialize.sql
+++ initialize/db_initialize.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE develop_database CHARACTER SET utf8mb4;
 show databases;
-GRANT ALL PRIVILEGES ON develop_database TO root@127.0.0.1;
-GRANT ALL PRIVILEGES ON develop_database TO root@192.168.33.10;
+use mysql;
+
+GRANT ALL PRIVILEGES ON *.* TO root@127.0.0.1;
+GRANT ALL PRIVILEGES ON *.* TO root@10.0.2.2;
+GRANT ALL PRIVILEGES ON *.* TO root@192.168.33.10;
+
 FLUSH PRIVILEGES;
